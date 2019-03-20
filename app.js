acb4bcc4ef8f4be6124c65cf97a7d24812c53ad6
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-20 16:04:59 -0700

    Commit 1-2 get even answered:

---
+++ app.js
@@ -0,0 +1,20 @@
+//Write a for loop that will log the numbers 1 through 20.
+
+// for(i=0;i<20;i++){
+//     console.log(i);
+// }
+
+//Write a for loop that will log only the even numbers in 0 through 200.
+// for(i=1;i<200;i++){
+//     if(i %2 === 0){
+//         console.log(i)
+//     }
+// }
+
+
+//Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+for(i=0;i<20;i++){
+     console.log(i);
+ }
+
+//For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
