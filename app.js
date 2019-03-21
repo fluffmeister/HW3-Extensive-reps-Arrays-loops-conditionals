05d477903c329dc90f9a8757818115d01de8e376
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-20 21:03:28 -0700

    Commit 5 - Getting to Know You answered & fizzbuzz updated

--- app.js
+++ app.js
@@ -22,12 +22,13 @@
 // "...the catnip made me do it...", 
 //or "...why does the red dot always get away..." 
 //at random.
-let cat=["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
- for(i=0;i<20;i++){
-    if(i %2==0)
-    Math.random(2);
-    console.log(cat.Math);
- }
+// let cat=["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+ 
+// for(i=0;i<20;i++){
+//     if(i %2==0)
+//     Math.random(2);
+//     console.log(cat.Math();
+//  }
 
 ////Write a javascript application that logs all numbers from 1 - 100.
 
@@ -47,10 +48,12 @@ let cat=["...human...why you taking pictures of me?...", "...the catnip made me 
 
 //If a number is divisible by 5 log "Buzz" instead of the number.
 // for(i=1;i<=100;i++){
-//     if(i %3==0){
+//    if(i % 3 === 0 && i % 5 ===0){
+//        console.log("FizzBuzz");
+//     else if(i %3==0){
 //         console.log('fizz')
 //         }
-//     if(i %5==0){
+//     else if(i %5==0){
 //     console.log('buzz')
 //     }
    
@@ -58,3 +61,37 @@ let cat=["...human...why you taking pictures of me?...", "...the catnip made me 
 //         console.log(i)
 //     }
 // }
+
+
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
+
+//1
+// kenny[0] = ["Gameboy"];
+// console.log(kenny[0]);
+//2
+// var JimAge= jimClark[1];
+// var NewJimAge= JimAge+1;
+// jimClark.push[NewJimAge];
+//3
+// ryan[0,2]= ["Batman","Gotham"];
+// console.log(ryan[0,2]);
+//4
+// reuben.pop();
+// console.log(reuben);
+// reuben.push("Chicago");
+// console.log(reuben);
+//5
+// jimHaff.pop();
+// jimHaff.push("Los Angeles","New York","Sacramento");
+// console.log(jimHaff);
+//Bonus
+// jimHaff.splice(2,1)
+// console.log(jimHaff);
+
+//Ninja Turtles
+
+const 
