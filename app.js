3f526c9c87d13ad23fa9b2f7913e2141df39f5fe
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-20 20:18:07 -0700

    Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -22,9 +22,39 @@
 // "...the catnip made me do it...", 
 //or "...why does the red dot always get away..." 
 //at random.
+let cat=["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+ for(i=0;i<20;i++){
+    if(i %2==0)
+    Math.random(2);
+    console.log(cat.Math);
+ }
 
-for(i=0;i<20;i++){
-    Math.random(3)
+////Write a javascript application that logs all numbers from 1 - 100.
 
-    console.log();
-}
+// for(i=1;i<=100;i++){
+//     console.log(i)
+// }
+
+//If a number is divisible by 3 log "Fizz" instead of the number.
+// for(i=1;i<=100;i++){
+//     if(i %3==0){
+//     console.log('fizz')
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+//If a number is divisible by 5 log "Buzz" instead of the number.
+// for(i=1;i<=100;i++){
+//     if(i %3==0){
+//         console.log('fizz')
+//         }
+//     if(i %5==0){
+//     console.log('buzz')
+//     }
+   
+//     else{
+//         console.log(i)
+//     }
+// }
